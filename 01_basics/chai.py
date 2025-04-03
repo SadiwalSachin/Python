9cddf4440d7cd00cd671188841b45f47855caf6b
Sachin Sadiwal <sachindeveloper.ch@gmail.com>
2025-04-03 21:39:16 +0530

    learinig started

---
+++ 01_basics/chai.py
@@ -0,0 +1,4 @@
+from hello_chai import chai
+#  importing files like this
+
+chai("Imported in chai.py file")
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,12 @@
+print("Chai or python")
+
+#  function defined
+def chai(n):
+    print(n)
+
+chai("lemon tea")
+
+#  variable also called attribute
+chai_one = "lemon tea"
+chai_two = "ginger tea"
+chai_three = "masala chai"
